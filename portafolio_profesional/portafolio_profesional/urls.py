3ed891d792ec4e20237469bf31041f69123cac8d
Camilo Toro <camilotoro@email.edu.co>
2026-03-26 16:21:54 -0500

    fix: implementación de menú global index.html y corrección de jerarquía de rutas

--- portafolio_profesional/portafolio_profesional/urls.py
+++ portafolio_profesional/portafolio_profesional/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # RUTA RAÍZ: Muestra el menú de los 4 integrantes
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    
+    # RUTAS DE CADA INTEGRANTE
     path('dev1/', include('dev1.urls')),
     path('dev2/', include('dev2.urls')),
-    path('dev3/', include("dev3.urls")), 
-    path('dev4/', include("dev4.urls")),
+    path('dev3/', include('dev3.urls')),
+    path('dev4/', include('dev4.urls')), 
 ]
